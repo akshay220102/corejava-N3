4ebfa8c1224d10fcd7e34975688e00d6e18425a9
paksh <paksh@Akshay>
2023-02-02 13:27:38 +0530

    second commit

--- dayone/src/org/tnsif/intro/HelloWorldDemo.java
+++ dayone/src/org/tnsif/intro/HelloWorldDemo.java
@@ -1,10 +1,10 @@
 package org.tnsif.intro;
 
-public class OperatorDemo {
+public class HelloWorldDemo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		System.out.println("Hello World!");
 	}
 
 }
