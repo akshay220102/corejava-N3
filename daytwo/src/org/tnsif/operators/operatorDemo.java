0ec90feb19d201d2e1b6a7951d07329854c36667
paksh <paksh@Akshay>
2023-02-02 14:04:14 +0530

    first commit

---
+++ daytwo/src/org/tnsif/operators/operatorDemo.java
@@ -0,0 +1,39 @@
+package org.tnsif.operators;
+import java.util.Scanner;
+public class operatorDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the value of x and y");
+		int x=sc.nextInt();
+		int y=sc.nextInt();
+		//Arithmetic Operator
+		System.out.println(x+y);
+		System.out.println(x-y);
+		System.out.println(x*y);
+		System.out.println(x/y);
+		//relational Operator
+		boolean res=x>y;
+		System.out.println(res);
+		//Assignment Operator
+		x*=y;
+		System.out.println(x);
+		//logical Operator
+		boolean res2=(5==5)&&(5>3);
+		boolean res3=(5==4)||(5>3);
+		boolean res4=!(5==4);
+		System.out.println(res2);
+		System.out.println(res3);
+		System.out.println(res4);
+		//bitwise
+		int a=12 &5;
+		int b=12|5;
+		int c=12^5;
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		
+	}
+
+}
