7434b219a634c02a1365a8606dc550d75fb3c7d4
paksh <paksh@Akshay>
2023-02-03 13:53:44 +0530

    third commit

--- daytwo/src/org/tnsif/jumping/ContinueDemo.java
+++ daytwo/src/org/tnsif/jumping/ContinueDemo.java
@@ -1,15 +1,14 @@
 package org.tnsif.jumping;
 
-public class BreakDemo {
+public class ContinueDemo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		for(int i=1;i<=10;i++) {
-			if(i==4) {
-				
-			break;}
-			System.out.println(i+ " ");
-		}
+			if(i==5) {
+				continue;
+			}
+		System.out.println(i);
 	}
-
+}
 }
